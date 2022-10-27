55377130e2d5090c4f4e086d99687807df46daac
Thanh Hoàng <fightforml@gmail.com>
2022-10-28 04:57:01 +0700

    add comment

--- reactjs/thanh-hoang/src/pages/LifeCyclePage/index.js
+++ reactjs/thanh-hoang/src/pages/LifeCyclePage/index.js
@@ -3,24 +3,34 @@ import { Component } from 'react'
 class LifeCyclePage extends Component {
   constructor(props) {
     super(props)
-    this.state = { index: 0 }
+    this.state = { index: 0, name: 'Thanh Hoang' }
   }
+  // *INFO: This method will update the state and trigger the render method again to update the DOM
   changeState() {
     this.setState({ index: this.state.index + 1 })
+    this.setState({ name: 'Thanh Hoang 2' })
   }
+  // *INFO: This method will be called before the component is mounted
   componentWillMount() {
     console.log('component Will Mount')
   }
+  // *INFO: This method will be called after the component is mounted
   componentDidMount() {
     console.log('component Did Mount')
   }
+  // *INFO: This method will be called when the component have some changes in the props or state
+  // *INFO: This method will return a boolean value, if it returns true, the component will be updated, otherwise it will not be updated
   shouldComponentUpdate(nextProps, nextState) {
+    // this.state.index = 1
     console.log('should Component Update with nextProps, nextState', nextProps, nextState)
+    //* INFO: Only update the component if the index is even
     return nextState.index % 2 === 0
   }
+  // *INFO: This method will be called before the component is updated
   componentWillUpdate() {
     console.log('component Will Update with state', this.state)
   }
+  // *INFO: This method will be called after the component is updated
   componentDidUpdate() {
     console.log('component Did Update with state', this.state)
   }
@@ -28,8 +38,9 @@ class LifeCyclePage extends Component {
   render() {
     return (
       <div>
-        <h1>{this.state.index}</h1>
-        <button onClick={this.changeState.bind(this)}>Click me</button>
+        <h1>This is Life cycle page</h1>
+        <h2>Index: {this.state.index}</h2>
+        <button onClick={this.changeState.bind(this)}>Increase index</button>
       </div>
     )
   }
--- reactjs/thanh-hoang/src/pages/UseStatePage/index.tsx
+++ reactjs/thanh-hoang/src/pages/UseStatePage/index.tsx
@@ -1,15 +1,24 @@
 import { useState } from 'react'
 
+//*INFO: component re-renders don't re-declare this variable, and it doesn't get re-initialized
+//*INFO: or re-assigned or re-declared or re-created or re-constructed or re-allocated or re-anything
+//*INFO: it's just a variable that's declared outside of the component and doesn't trigger a re-render
 let wrongCount: number = 0
 const UseStatePage = () => {
+  //*INFO: component will re-render when this variable changes
   let [count, setCount] = useState(0)
+
+  //*INFO: component will not re-render when this variable changes
+  //*INFO: component re-renders re-declare this variable, so it will be reset to 0
   let wrongCountInside: number = 0
   console.log('🚀 ~ count, wrongCount, wrongCountInside: ', count, wrongCount, wrongCountInside)
 
+  //*INFO: return in a component is a render
   return (
     <div>
       THIS IS USE STATE PAGE
       <div>
+        {/* //*INFO: It only change when component re-render */}
         <p>Count: {count}</p>
         <button
           onClick={() => {
